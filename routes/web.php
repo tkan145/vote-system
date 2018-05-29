929afafeeab97b4e2f23853f0cec01f2c870d090
ant <tkan145@gmail.com>
2018-05-29 15:37:55 +1000

    Updated route

--- routes/web.php
+++ routes/web.php
@@ -12,22 +12,95 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
 
-Route::get('/create-vote', function () {
-  return view('create-vote');
-})->name('vote.create');
-
 Route::get('/signin', function () {
-  return view('signin');
+  return view('pages.signin');
 })->name('auth.signin');
 
 Route::get('/register', function () {
-  return view('register');
+  return view('pages.register');
 })->name('auth.register');
 
-Route::get('/vote', function () {
-  return view('vote');
+Route::get('/votes', function () {
+  return view('pages.votes');
 })->name('vote.voting');
+
+Route::get('/about', function () {
+  return view('pages.about');
+})->name('other.about');
+
+Route::get('votes/{id}',function($id){
+
+  if($id == 1){
+    $post = [
+      'title' => 'Vote Title 1',
+      'content' => 'this is the options list'
+    ];
+  }else {
+    $post = [
+      'title' => 'Vote Title 2',
+      'content' => 'this is the options list'
+    ];
+  }
+  return view('pages.vote', ['post' => $post]);
+})->name('vote.page');
+
+Route::group(['prefix' => 'admin'],function(){
+
+  Route::get('','VoteController@getIndex')->name('admin.index');
+  // Route::get('', function () {
+  //   return view('pages.index');
+  // })->name('admin.index');
+
+  Route::get('create-vote', function () {
+    return view('pages.create-vote');
+  })->name('admin.create');
+
+  Route::post('create-vote',function(\Illuminate\Http\Request $request, \Illuminate\Http\Validation\Factory $validator){
+    $valid = $validator->make($request->all(),[
+      'title' => 'required|min:5',
+    ]);
+
+    if($valid->fails()){
+      return redirect()->back()->withErrors($valid);
+    }
+
+    return redirect()-route('admin.index')->with('info','Post created, new Title: ' . $request->input('title'));
+  })->name('admin.create');
+
+  Route::get('edit/{id}', function ($id) {
+
+    if($id == 1){
+      $post = [
+        'title' => 'Vote Title 1',
+        'content' => 'this is the options list'
+      ];
+    }else {
+      $post = [
+        'title' => 'Vote Title 2',
+        'content' => 'this is the options list'
+      ];
+    }
+
+    return view('pages.edit-vote',['post' => $post]);
+  })->name('admin.edit');
+
+  Route::post('admin/edit/', function (\Illuminate\Http\Request $request, \Illuminate\Http\Validation\Factory $validator) {
+    $valid = $validator->make($request->all(),[
+      'title' => 'required|min:5',
+    ]);
+
+    if($valid->fails()){
+      return redirect()->back()->withErrors($valid);
+    }
+
+    return redirect()-route('admin.index')->with('info','Post edited, new Title: ' . $request->input('title'));
+  })->name('admin.update');
+});
+
+
+
+
 
