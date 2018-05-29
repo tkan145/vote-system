240bbdcaeb601a90b6c9ada80c24289049d45f0e
ant <tkan145@gmail.com>
2018-05-29 16:16:35 +1000

    Updated Vote model with get, edit, add

--- app/Vote.php
+++ app/Vote.php
@@ -11,6 +11,28 @@ class Vote {
     return $session->get('votes');
   }
 
+  public function getVote($session, $id){
+    if( !$session->has('votes')){
+      $this->createDummyData($session);
+    }
+    return $session->get('votes')[$id];
+  }
+
+  public function addVote($session,$title,$content){
+    if( !$session->has('votes')){
+      $this->createDummyData($session);
+    }
+    $votes = $session->get('votes');
+    array_push($votes,['title' => $title, 'content' => $content]);
+    $session->put('votes',$votes);
+  }
+
+  public function editVote($session,$id,$title,$content){
+    $votes = $session->get('votes');
+    $votes[$id] = ['title' => $title, 'content' => $content];
+    $session->put('votes',$votes);
+  }
+
   private function createDummyData($session){
     $votes = [
       [
