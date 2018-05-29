2654e226cfaed9c8d3e7dd5bc80c01cae5f7b6a5
ant <tkan145@gmail.com>
2018-05-29 13:19:00 +1000

    Added header for admin page

---
+++ resources/views/includes/admin-header.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white flex-row flex-md-row shadow bd-navbar">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/" aria-label="PY">Vote System</a>
+  <ul class="navbar-nav flex-row ml-md-auto d-md-flex d-none">
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profile</a>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Settings</a>
+        <a class="dropdown-item" href="#">About</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Sign out</a>
+      </div>
+    </li>
+  </ul>
+  <!-- <a class="btn d-none d-lg-inline-block mb-3 mb-md-0 ml-md-3">Profile</a> -->
+</nav>
