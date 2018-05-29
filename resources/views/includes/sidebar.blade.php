a68122a809f857c5bd0ae45b17efd64938e4e22e
ant <tkan145@gmail.com>
2018-05-29 13:02:37 +1000

    Added sidebar

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,26 @@
+<div class="sidebar-sticky">
+  <div class="row mt-5 align-center text-center">
+    <div class="col-12">
+      <a class="btn btn-block btn-primary" href="/admin/create-vote">Create New vote</a>
+    </div>
+  </div>
+  <h5 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-3 mb-1 text-muted">
+    <span>Dashboard</span>
+  </h5>
+  <ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link" href="/admin">
+        <span data-feather="home"></span>
+        Home <span class="sr-only">(current)</span>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="/create-vote">
+        <span data-feather="home"></span>
+        Results <span class="sr-only">(current)</span>
+      </a>
+    </li>
+  </ul>
+
+</div>
