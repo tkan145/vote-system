3d4dc8fd52256b583d340e27e655a3a13bdf0547
ant <tkan145@gmail.com>
2018-05-30 17:20:20 +1000

    Remove sessions from Vote Model

--- app/Vote.php
+++ app/Vote.php
@@ -2,49 +2,11 @@
 
 namespace App;
 
-class Vote {
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-  public function getVotes($session){
-    if( !$session->has('votes')){
-      $this->createDummyData($session);
-    }
-    return $session->get('votes');
-  }
+class Vote extends Model {
 
-  public function getVote($session, $id){
-    if( !$session->has('votes')){
-      $this->createDummyData($session);
-    }
-    return $session->get('votes')[$id];
-  }
-
-  public function addVote($session,$title,$content){
-    if( !$session->has('votes')){
-      $this->createDummyData($session);
-    }
-    $votes = $session->get('votes');
-    array_push($votes,['title' => $title, 'content' => $content]);
-    $session->put('votes',$votes);
-  }
-
-  public function editVote($session,$id,$title,$content){
-    $votes = $session->get('votes');
-    $votes[$id] = ['title' => $title, 'content' => $content];
-    $session->put('votes',$votes);
-  }
-
-  private function createDummyData($session){
-    $votes = [
-      [
-        'title'   => 'Vote topic 1',
-        'content' => 'this is the options list'
-      ],
-      [
-        'title'   => 'Vote topic 2',
-        'content' => 'this is the options list'
-      ]
-      ];
-
-      $session->put('votes',$votes);
-  }
+  protected $fillable = ['title', 'description'];
+  //use SoftDeletes;
 }
